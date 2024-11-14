6b6c98f80945997364b2e4aeaf8612734856b3dc
Emanoel Figueredo <emanoel.figueredo@atan.com.br>
2024-11-14 14:40:05 -0300

    ci: :wrench:

--- frontend/src/app/cadastro-cliente/services/api-cnpj.service.ts
+++ frontend/src/app/cadastro-cliente/services/api-cnpj.service.ts
@@ -11,8 +11,7 @@ export class ApiCnpjService {
   constructor(private readonly httpClient: HttpClient) { }
 
   public consultarCNPJ(cnpj: string): Observable<ResponseApiCnpj> {
-    var url = `https://receitaws.com.br/v1/cnpj/${cnpj}`;
-    // var url = `/v1/cnpj/${cnpj}`;
+    var url = `/v1/cnpj/${cnpj}`;
     return this.httpClient.get<ResponseApiCnpj>(url);
   }
 }
