42b66d8215f55e08df22f44a24443f106301ea34
Emanoel Figueredo <emanoel.figueredo@atan.com.br>
2024-11-14 16:24:02 -0300

    .

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MensagemCampoObrigatorioComponent } from './componentes/mensagem-campo-obrigatorio/mensagem-campo-obrigatorio.component';
 import { MessagesModule } from 'primeng/messages';
 import { NotFound404Component } from './componentes/page/not-found-404/not-found-404.component';
-import { BreadCrumbComponent } from './componentes/bread-crumb/bread-crumb.component';
 import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { BreadCrumbComponent } from '../componentes/bread-crumb/bread-crumb.component';
 
 @NgModule({
   declarations: [
-    MensagemCampoObrigatorioComponent,
     NotFound404Component,
     BreadCrumbComponent,
   ],
   imports: [CommonModule, MessagesModule, BreadcrumbModule],
   exports: [
-    MensagemCampoObrigatorioComponent,
     NotFound404Component,
     BreadCrumbComponent,
   ],
