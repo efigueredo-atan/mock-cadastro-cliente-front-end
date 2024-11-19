58ec95af2ad58824c2f001497348a075813f0f3a
Emanoel Figueredo <emanoel.figueredo@atan.com.br>
2024-11-19 11:41:41 -0300

    feat: :sparkles: Implementar logica de alterar quantidade de produtos no pedido

--- frontend/src/app/produtos-pedidos/componentes/core/produto-card-small/produto-card-small.component.ts
+++ frontend/src/app/produtos-pedidos/componentes/core/produto-card-small/produto-card-small.component.ts
@@ -4,8 +4,17 @@ import { Produto, ProdutoAtendimento } from '../../../../shared/types/types';
 @Component({
   selector: 'app-produto-card-small',
   templateUrl: './produto-card-small.component.html',
-  styleUrl: './produto-card-small.component.css'
+  styleUrl: './produto-card-small.component.css',
 })
 export class ProdutoCardSmallComponent {
   @Input() public produtoAtendimento: ProdutoAtendimento;
+
+  public aumentarQuantidadeItem(): void {
+    this.produtoAtendimento.qtdAtendimento++;
+  }
+  public diminuirQuantidadeItem(): void {
+    if (this.produtoAtendimento.qtdAtendimento > 1) {
+      this.produtoAtendimento.qtdAtendimento--;
+    }
+  }
 }
